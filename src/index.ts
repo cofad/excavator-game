f685edfe3395dbe7aadc7f8bbe205e496feacf6c
Will Warner <will.r.warner@gmail.com>
2021-07-18 13:20:39 -0500

    feat: dynamically adjust canvas size

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-const canvas: HTMLCanvasElement = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+function main() {
+  updateCanvasSize();
+  render();
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function render() {
+  updateCanvasSize();
+  drawRectangle();
+  requestAnimationFrame(render);
+}
+
+function drawRectangle() {
+  const canvas: HTMLCanvasElement = document.querySelector('#canvas');
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.rect(10, 10, 20, 20);
+  ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function updateCanvasSize() {
+  const canvas: HTMLCanvasElement = document.querySelector('#canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+main();
